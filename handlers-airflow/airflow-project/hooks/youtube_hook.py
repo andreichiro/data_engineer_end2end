ac4a294d5f6fe49af75fa68ff6ec74b5cb477602
André <andreichiro@gmail.com>
2023-07-16 16:50:56 -0300

    Update youtube_hook.py
    Updating imports due to airflow config

--- handlers-airflow/airflow-project/hooks/youtube_hook.py
+++ handlers-airflow/airflow-project/hooks/youtube_hook.py
@@ -1,5 +1,5 @@
 from googleapiclient.discovery import build
-from abstract_classes.abstract_hook import AbstractHook
+from ..abstract_classes.abstract_hook import AbstractHook
 
 class YouTubeHook(AbstractHook):
     def __init__(self, api_key, api_service_name, api_version):

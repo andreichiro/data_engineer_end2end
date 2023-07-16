08769c0726440fa8219cc29f671e2ea471c0cf40
André <andreichiro@gmail.com>
2023-07-16 18:00:49 -0300

    Update pandas_processor.py
    Uploading imports

--- handlers-airflow/airflow-project/processors/pandas_processor.py
+++ handlers-airflow/airflow-project/processors/pandas_processor.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from ..abstract_classes.abstract_processor import AbstractProcessor
+from dags.abstract_classes.abstract_processor import AbstractProcessor
 
 class PandasProcessor(AbstractProcessor):
     def process(self, video_details, file_path):

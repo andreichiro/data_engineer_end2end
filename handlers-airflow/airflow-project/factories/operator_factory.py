bea3943eb020aeb62a34ff8ea7eb2502ba5c7995
André <andreichiro@gmail.com>
2023-07-17 15:22:27 -0300

    Update operator_factory.py
    Updating operator factory

--- handlers-airflow/airflow-project/factories/operator_factory.py
+++ handlers-airflow/airflow-project/factories/operator_factory.py
@@ -1,5 +1,5 @@
-from dags.operators.aws_operator import AWSOperator
-from dags.operators.youtube_operator import YouTubeOperator
+from operators.aws_operator import AWSOperator
+from operators.youtube_operator import YouTubeOperator
 
 class OperatorFactory:
     @staticmethod
